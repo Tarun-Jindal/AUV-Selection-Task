4ad226fac8c2de96e6157117fc1a8f289e25a667
Tarun Jindal <43723647+Tarun-Jindal@users.noreply.github.com>
2019-03-13 19:10:56 +0000

    Add files via upload

---
+++ catkin_ws4/src/actionlib_task1/src/newtonraphson_client-updated.cpp
@@ -0,0 +1,79 @@
+﻿
+#include <ros/ros.h>
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib_task1/NewtonRaphsonAction.h>
+#include <iostream>
+
+using namespace actionlib_task1;
+
+double ratio;
+
+typedef actionlib::SimpleActionClient<NewtonRaphsonAction> Client;
+
+// Called once when the goal completes
+void doneCb(const actionlib::SimpleClientGoalState& state,
+            const NewtonRaphsonResultConstPtr& result)
+{
+  ROS_INFO("Finished in state [%s]", state.toString().c_str());
+ // ROS_INFO("Answer: %i", result->ratio.back());
+  ROS_INFO("Answer: %lf", result->ratio);
+  ros::shutdown();
+}
+
+// Called once when the goal becomes active
+void activeCb()
+{
+  ROS_INFO("Goal is active");
+}
+
+// Called every time feedback is received for the goal
+void feedbackCb(const NewtonRaphsonFeedbackConstPtr& feedback)
+{
+  //ROS_INFO("Got Feedback of length %lf", feedback->ratio.size());
+  ROS_INFO("Got Feedback of length %lf", feedback->ratio);
+  ratio = feedback->ratio;
+}
+
+int main (int argc, char **argv)
+{
+  ros::init(argc, argv, "test_newtonraphson_ratio");
+
+  // Create the action client
+  Client ac("newtonraphson", true);
+
+  ROS_INFO("Waiting for action server to start.");
+  ac.waitForServer();
+  ROS_INFO("Action server started, sending goal.");
+
+  // Send Goal
+  NewtonRaphsonGoal goal;
+  goal.x = 10;
+  ratio = 3;
+  while(ratio > 0.000001)
+  {
+    goal.x = goal.x - ratio;
+  
+  ac.sendGoal(goal, &doneCb, &activeCb, &feedbackCb);
+ //wait for the action to return
+  bool finished_before_timeout = ac.waitForResult(ros::Duration(10.0));
+
+  if (finished_before_timeout)
+  {
+    actionlib::SimpleClientGoalState state = ac.getState();
+    ROS_INFO("Action finished: %s",state.toString().c_str());
+  }
+  else
+    ROS_INFO("Action did not finish before the time out.");
+
+  
+}
+  
+ROS_INFO("Final root is  %lf ", goal.x);
+  
+
+ros::spin();
+ 
+ 
+
+  return 0;
+}
---
+++ catkin_ws4/src/actionlib_task1/src/newtonraphson_server-updated.cpp
@@ -0,0 +1,91 @@
+﻿
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include <actionlib_task1/NewtonRaphsonAction.h>
+#include <cmath>
+#include <iostream>
+
+class NewtonRaphsonAction
+{
+protected:
+
+  ros::NodeHandle nh_;
+  actionlib::SimpleActionServer<actionlib_task1::NewtonRaphsonAction> as_; // NodeHandle instance must be created before this line. Otherwise strange error occurs.
+  std::string action_name_;
+  // create messages that are used to published feedback/result
+  actionlib_task1::NewtonRaphsonFeedback feedback_;
+  actionlib_task1::NewtonRaphsonResult result_;
+
+public:
+
+  NewtonRaphsonAction(std::string name) :
+    as_(nh_, name, boost::bind(&NewtonRaphsonAction::executeCB, this, _1), false),
+    action_name_(name)
+  {
+    as_.start();
+  }
+
+  ~NewtonRaphsonAction(void)
+  {
+  }
+
+  void executeCB(const actionlib_task1::NewtonRaphsonGoalConstPtr &goal)
+  {
+    // helper variables
+    ros::Rate r(1);
+    bool success = true;
+
+    // push_back the seeds for the newtonraphson ratio
+    //feedback_.ratio.clear();
+    //feedback_.ratio.push_back(0);
+    //feedback_.ratio.push_back(1);
+
+    // publish info to the console for the user
+    ROS_INFO("%s: Executing, creating newtonraphson ratio : x = %lf", action_name_.c_str(), goal->x);
+
+    // start executing the action
+       double x = goal -> x ;
+    
+       double f = pow(x,3) − 5*x + 13;
+    
+       double f1 = 3*pow(x,2) - 5;
+    
+       double ratio =​ (f)/(f1);
+
+      // check that preempt has not been requested by the client      if (as_.isPreemptRequested() || !ros::ok())
+      /*{
+        ROS_INFO("%s: Preempted", action_name_.c_str());
+        // set the action state to preempted
+        as_.setPreempted();
+        success = false;
+        break;
+      }*/
+      feedback_.ratio = ratio;
+      // publish the feedback
+      as_.publishFeedback(feedback_);
+      // this sleep is not necessary, the ratio is computed at 1 Hz for demonstration purposes
+      r.sleep();
+    
+
+    if(success)
+    {
+      result_.ratio = feedback_.ratio;
+      ROS_INFO("%s: Succeeded", action_name_.c_str());
+      // set the action state to succeeded
+      as_.setSucceeded(result_);
+    }
+  }
+
+
+};
+
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "newtonraphson");
+
+  NewtonRaphsonAction newtonraphson("newtonraphson");
+  ros::spin();
+
+  return 0;
+}
